e6bb6cbe978e14eac46e468be48a3ae27df2c51e
Nemesis Sierra Aranda <81599453+Burnestein@users.noreply.github.com>
2021-10-28 21:25:42 -0500

    Add project files.

---
+++ DlgMenuPrincipal.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace SSPP21B_ProyectoFinal_NemesisSIerra
+{
+    partial class DlgProyectoFinal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DlgProyectoFinal));
+            this.PnlBotones = new System.Windows.Forms.Panel();
+            this.BtnPromociones = new System.Windows.Forms.Button();
+            this.BtnFuenteSodas = new System.Windows.Forms.Button();
+            this.BtnCartelera = new System.Windows.Forms.Button();
+            this.PnlLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PnlSuperior = new System.Windows.Forms.Panel();
+            this.BtnConfiguracion = new System.Windows.Forms.Button();
+            this.LblCaja = new System.Windows.Forms.Label();
+            this.MstPrincipal = new System.Windows.Forms.MenuStrip();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.VolverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PnlBotones.SuspendLayout();
+            this.PnlLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.PnlSuperior.SuspendLayout();
+            this.MstPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlBotones
+            // 
+            this.PnlBotones.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.PnlBotones.Controls.Add(this.BtnPromociones);
+            this.PnlBotones.Controls.Add(this.BtnFuenteSodas);
+            this.PnlBotones.Controls.Add(this.BtnCartelera);
+            this.PnlBotones.Controls.Add(this.PnlLogo);
+            this.PnlBotones.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlBotones.Location = new System.Drawing.Point(0, 30);
+            this.PnlBotones.Name = "PnlBotones";
+            this.PnlBotones.Size = new System.Drawing.Size(230, 723);
+            this.PnlBotones.TabIndex = 0;
+            // 
+            // BtnPromociones
+            // 
+            this.BtnPromociones.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnPromociones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnPromociones.FlatAppearance.BorderSize = 0;
+            this.BtnPromociones.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SlateBlue;
+            this.BtnPromociones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPromociones.Font = new System.Drawing.Font("Microsoft JhengHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPromociones.ForeColor = System.Drawing.Color.White;
+            this.BtnPromociones.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnPromociones.Location = new System.Drawing.Point(0, 280);
+            this.BtnPromociones.Name = "BtnPromociones";
+            this.BtnPromociones.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.BtnPromociones.Size = new System.Drawing.Size(230, 70);
+            this.BtnPromociones.TabIndex = 3;
+            this.BtnPromociones.Text = "Promociones";
+            this.BtnPromociones.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnPromociones.UseVisualStyleBackColor = true;
+            // 
+            // BtnFuenteSodas
+            // 
+            this.BtnFuenteSodas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnFuenteSodas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnFuenteSodas.FlatAppearance.BorderSize = 0;
+            this.BtnFuenteSodas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SlateBlue;
+            this.BtnFuenteSodas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFuenteSodas.Font = new System.Drawing.Font("Microsoft JhengHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFuenteSodas.ForeColor = System.Drawing.Color.White;
+            this.BtnFuenteSodas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnFuenteSodas.Location = new System.Drawing.Point(0, 210);
+            this.BtnFuenteSodas.Name = "BtnFuenteSodas";
+            this.BtnFuenteSodas.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.BtnFuenteSodas.Size = new System.Drawing.Size(230, 70);
+            this.BtnFuenteSodas.TabIndex = 2;
+            this.BtnFuenteSodas.Text = "Fuente de Sodas";
+            this.BtnFuenteSodas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnFuenteSodas.UseVisualStyleBackColor = true;
+            // 
+            // BtnCartelera
+            // 
+            this.BtnCartelera.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnCartelera.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnCartelera.FlatAppearance.BorderSize = 0;
+            this.BtnCartelera.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SlateBlue;
+            this.BtnCartelera.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCartelera.Font = new System.Drawing.Font("Microsoft JhengHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCartelera.ForeColor = System.Drawing.Color.White;
+            this.BtnCartelera.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnCartelera.Location = new System.Drawing.Point(0, 140);
+            this.BtnCartelera.Name = "BtnCartelera";
+            this.BtnCartelera.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.BtnCartelera.Size = new System.Drawing.Size(230, 70);
+            this.BtnCartelera.TabIndex = 1;
+            this.BtnCartelera.Text = "Cartelera";
+            this.BtnCartelera.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnCartelera.UseVisualStyleBackColor = true;
+            this.BtnCartelera.Click += new System.EventHandler(this.BtnCartelera_Click);
+            // 
+            // PnlLogo
+            // 
+            this.PnlLogo.Controls.Add(this.pictureBox1);
+            this.PnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.PnlLogo.Name = "PnlLogo";
+            this.PnlLogo.Size = new System.Drawing.Size(230, 140);
+            this.PnlLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PnlSuperior
+            // 
+            this.PnlSuperior.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.PnlSuperior.Controls.Add(this.BtnConfiguracion);
+            this.PnlSuperior.Controls.Add(this.LblCaja);
+            this.PnlSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlSuperior.Location = new System.Drawing.Point(230, 30);
+            this.PnlSuperior.Name = "PnlSuperior";
+            this.PnlSuperior.Size = new System.Drawing.Size(952, 100);
+            this.PnlSuperior.TabIndex = 1;
+            // 
+            // BtnConfiguracion
+            // 
+            this.BtnConfiguracion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnConfiguracion.BackColor = System.Drawing.Color.SlateBlue;
+            this.BtnConfiguracion.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnConfiguracion.BackgroundImage")));
+            this.BtnConfiguracion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnConfiguracion.FlatAppearance.BorderSize = 0;
+            this.BtnConfiguracion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnConfiguracion.Location = new System.Drawing.Point(927, 0);
+            this.BtnConfiguracion.Name = "BtnConfiguracion";
+            this.BtnConfiguracion.Size = new System.Drawing.Size(25, 25);
+            this.BtnConfiguracion.TabIndex = 1;
+            this.BtnConfiguracion.UseVisualStyleBackColor = false;
+            this.BtnConfiguracion.Click += new System.EventHandler(this.BtnConfiguracion_Click);
+            // 
+            // LblCaja
+            // 
+            this.LblCaja.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.LblCaja.AutoSize = true;
+            this.LblCaja.Font = new System.Drawing.Font("Microsoft JhengHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCaja.ForeColor = System.Drawing.Color.White;
+            this.LblCaja.Location = new System.Drawing.Point(803, 33);
+            this.LblCaja.Name = "LblCaja";
+            this.LblCaja.Size = new System.Drawing.Size(81, 30);
+            this.LblCaja.TabIndex = 0;
+            this.LblCaja.Text = "Caja 1";
+            // 
+            // MstPrincipal
+            // 
+            this.MstPrincipal.BackColor = System.Drawing.SystemColors.Control;
+            this.MstPrincipal.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MstPrincipal.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MstPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editarToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.VolverToolStripMenuItem});
+            this.MstPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.MstPrincipal.Name = "MstPrincipal";
+            this.MstPrincipal.Size = new System.Drawing.Size(1182, 30);
+            this.MstPrincipal.TabIndex = 2;
+            this.MstPrincipal.Text = "menuStrip1";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(62, 26);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(65, 26);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // VolverToolStripMenuItem
+            // 
+            this.VolverToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.VolverToolStripMenuItem.Name = "VolverToolStripMenuItem";
+            this.VolverToolStripMenuItem.Size = new System.Drawing.Size(135, 26);
+            this.VolverToolStripMenuItem.Text = "Volver a terminal";
+            this.VolverToolStripMenuItem.Click += new System.EventHandler(this.VolverToolStripMenuItem_Click);
+            // 
+            // DlgProyectoFinal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.PnlSuperior);
+            this.Controls.Add(this.PnlBotones);
+            this.Controls.Add(this.MstPrincipal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MstPrincipal;
+            this.MaximizeBox = false;
+            this.Name = "DlgProyectoFinal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cinemax || Punto de venta automático";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.PnlBotones.ResumeLayout(false);
+            this.PnlLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.PnlSuperior.ResumeLayout(false);
+            this.PnlSuperior.PerformLayout();
+            this.MstPrincipal.ResumeLayout(false);
+            this.MstPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlBotones;
+        private System.Windows.Forms.Button BtnPromociones;
+        private System.Windows.Forms.Button BtnFuenteSodas;
+        private System.Windows.Forms.Button BtnCartelera;
+        private System.Windows.Forms.Panel PnlLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel PnlSuperior;
+        private System.Windows.Forms.Label LblCaja;
+        private System.Windows.Forms.Button BtnConfiguracion;
+        private System.Windows.Forms.MenuStrip MstPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem VolverToolStripMenuItem;
+    }
+}
+
---
+++ DlgMenuPrincipal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSPP21B_ProyectoFinal_NemesisSIerra
+{
+    //---------------------------------------------------------------------
+    //Diálogo principal del proyecto final.
+    //NJSA. 08/09/2021.
+    //---------------------------------------------------------------------
+    public partial class DlgProyectoFinal : Form
+    {
+        //---------------------------------------------------------------------
+        //Atributos.
+        //---------------------------------------------------------------------
+
+        //---------------------------------------------------------------------
+        //Constructor.
+        //---------------------------------------------------------------------
+        public DlgProyectoFinal()
+        {
+            InitializeComponent();
+            this.ControlBox = false;
+            this.Text = string.Empty;
+            this.MstPrincipal.Visible = false;
+        }
+
+        private void BtnCartelera_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnConfiguracion_Click(object sender, EventArgs e)
+        {
+            this.ControlBox = true;
+            this.Text = "CINEMAX || Punto de venta automático";
+            this.MstPrincipal.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            Size = new Size(1200, 800);
+            CenterToScreen();
+
+         
+
+        }
+        private void VolverToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+            this.Text = string.Empty;
+            this.MstPrincipal.Visible = false;
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
